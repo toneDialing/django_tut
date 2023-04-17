a254b58137d4ade3b2f881d4633af11ca64a3543
Matthew Rathkey <rathkeym@gmail.com>
2023-04-17 11:34:18 -0600

    Add client/server validation to form

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-tasks = ["foo", "bar", "baz"]
+tasks = []
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
 
 # Create your views here.
 def index(request):
@@ -9,4 +15,16 @@ def index(request):
     })
 
 def add(request):
-    return render(request, "tasks/add.html")
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm()
+    })
